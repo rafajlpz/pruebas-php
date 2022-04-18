ee645c37bd00535eadbb6c16038c01a6a7fc4d49
rafajlpz <frilo_94@hotmail.com>
2022-04-18 14:38:58 +0200

    Añadido ejercicios php
    Completado ejercicio 1

---
+++ script1.php
@@ -0,0 +1,11 @@
+<?php
+//1. Crear una estructura html y mostrar en el título <title> el saludo "Hola tu nombre". El nombre debe aparecer mediante una variable
+
+$nombre = "Rafa";
+
+?>
+<title> 
+    
+    <?php echo 'Hola' $nombre;?>
+
+</title>
