36605fe97399ee6a5dec8bdcfb693c2ecd819428
rafajlpz <frilo_94@hotmail.com>
2022-04-22 10:13:26 +0200

    Finalizado script2

--- script1.php
+++ script1.php
@@ -1,11 +1,18 @@
 <?php
 //1. Crear una estructura html y mostrar en el título <title> el saludo "Hola tu nombre". El nombre debe aparecer mediante una variable
-
-$nombre = "Rafa";
-
+$nombre = 'Rafa';
 ?>
-<title> 
-    
-    <?php echo 'Hola'  $nombre;?>
 
-</title>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola <?php echo $nombre; ?></title>
+</head>
+<body>
+    
+</body>
+</html>
