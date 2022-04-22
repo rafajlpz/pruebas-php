470583a802332d622d04cd36cf91c5d4c89092b2
rafajlpz <frilo_94@hotmail.com>
2022-04-22 11:16:28 +0200

    Inicio script3

--- script3constantes.php
+++ script3constantes.php
@@ -3,3 +3,18 @@
 //y calcula el área de un círculo a partir de un radio dado
 
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web PHP</title>
+</head>
+<body>
+    <h1>Prueba PHP</h1>
+    <h2>Creando constante PI</h2>
+    <?php const PI = 3.1416 ?>
+</body>
+</html>
